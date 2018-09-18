7118a391f99e15e1cb65451eeab5a52ec2b7984d
manas <sahoodx@gmail.com>
2018-09-18 11:15:18 +0530

    commit

--- execution_script.R
+++ execution_script.R
@@ -9,20 +9,33 @@ load("~/JGL/example.data.rda")
 Y = example.data
 
 DATA = list()
-DATA[[1]] = read.csv("~/JGL_inter/gene_adip.csv")[,2:100]
-DATA[[2]] = read.csv("~/JGL_inter/gene_musc.csv")[,2:100]
-DATA[[3]] = read.csv("~/JGL_inter/gene_skin.csv")[,2:100]
-DATA[[4]] = read.csv("~/JGL_inter/gene_thyr.csv")[,2:100]
+DATA[[1]] = read.csv("~/JGL_inter/gene_adip.csv") ######1st column is identifier
+DATA[[2]] = read.csv("~/JGL_inter/gene_musc.csv")
+DATA[[3]] = read.csv("~/JGL_inter/gene_skin.csv")
+DATA[[4]] = read.csv("~/JGL_inter/gene_thyr.csv")
+DATA1 = list()
+DATA1[[1]] = DATA[[1]][,2:101]
+DATA1[[2]] = DATA[[1]][,2:101]
+DATA1[[3]] = DATA[[1]][,2:101]
+DATA1[[4]] = DATA[[1]][,2:101]
 K = 4
-p = dim(DATA[[1]])[2]
+p_o = dim(DATA[[1]])[2]
+p = dim(DATA1[[1]])[2]
 
-vardata = array(dim=c(K,p))
+vardata = array(dim=c(K,p_o-1))
 for(k in 1:K)
 {
-  for(j in 1:p)
+  for(j in 2:p_o)
   {
-    
-    #vardata[k,j] = var(DATA[[k]][,j])
+    vardata[k,j-1] = var(DATA[[k]][,j])
+  }
+}
+meandata = array(dim=c(1,p_o-1))
+for(k in 1:K)
+{
+  for(j in 1:p_o-1)
+  {
+    meandata[j] = mean(vardata[,j])
   }
 }
 
